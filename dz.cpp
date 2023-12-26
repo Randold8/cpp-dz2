a7b0cd2ee4eb1a7112ade95a6eeed553e3d91cf4
Randold8 <84187343+Randold8@users.noreply.github.com>
2023-12-26 03:25:16 +0300

    Create dz.cpp

---
+++ dz.cpp
@@ -0,0 +1,14 @@
+//1.1
+const float constValue = 3.14f;
+const float* const ptrToConstFloat = &constValue; // Константный указатель на константу типа float
+//1.2
+typedef long int* LongIntPtr; // Тип LongIntPtr теперь является указателем на long int
+LongIntPtr ptrToLongInt; // Объявляем переменную этого типа
+//1.3
+double* ptrToDouble; // Указатель на double
+//1.4
+const short int* ptrToConstShortInt;
+//1.5
+typedef const float* const ConstPtrToConstFloat;
+//1.6
+typedef const char* CharPtrToConst;
